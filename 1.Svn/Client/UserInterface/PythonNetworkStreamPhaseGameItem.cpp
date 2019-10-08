7690570f2256e173e399e0f1b2092626ba55d2ee
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-08 20:54:34 +0300

    Add files via upload

--- 1.Svn/Client/UserInterface/PythonNetworkStreamPhaseGameItem.cpp
+++ 1.Svn/Client/UserInterface/PythonNetworkStreamPhaseGameItem.cpp
@@ -3,11 +3,9 @@
 	
 ///Add
 #ifdef TOOLTIP_GROUND_ITEM
-	std::vector<long> sockets;
+	std::vector<long> sockets(packet_item_ground_add.alSockets, packet_item_ground_add.alSockets + ITEM_SOCKET_SLOT_MAX_NUM);
 	std::vector<BYTE> attrtype;
 	std::vector<short> attrvals;
-	for (auto i = 0; i < ITEM_SOCKET_SLOT_MAX_NUM; i++)
-		sockets.emplace_back(packet_item_ground_add.alSockets[i]);
 	for (auto i = 0; i < ITEM_ATTRIBUTE_SLOT_MAX_NUM; i++) {
 		attrtype.emplace_back(packet_item_ground_add.aAttr[i].bType);
 		attrvals.emplace_back(packet_item_ground_add.aAttr[i].sValue);
@@ -22,13 +20,10 @@
 									   packet_item_ground_add.lZ);
 	
 ///Change
-	CPythonItem::Instance().CreateItem(packet_item_ground_add.dwVID,
-									   packet_item_ground_add.dwVnum,
+	CPythonItem::Instance().CreateItem(packet_item_ground_add.dwVID,packet_item_ground_add.dwVnum,
 									#ifdef TOOLTIP_GROUND_ITEM
 										sockets,
 										attrtype,
 										attrvals,
 									#endif
-									   packet_item_ground_add.lX,
-									   packet_item_ground_add.lY,
-									   packet_item_ground_add.lZ);
+										packet_item_ground_add.lX,packet_item_ground_add.lY,packet_item_ground_add.lZ);
