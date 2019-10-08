9722b6bf0d8523d5ec22badc6faa0912a65bb362
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-08 21:51:18 +0300

    Add files via upload

--- 2.Client/root/game.py
+++ 2.Client/root/game.py
@@ -5,7 +5,7 @@
 		
 #Add
 	if app.TOOLTIP_GROUND_ITEM:
-		def ShowItemFromClient(self, open, itemvnum = 0, metinslot = 0, attr = 0):
+		def ShowItemFromClient(self, open, itemvnum = 0, metinslot = 0, attrtype = 0, attrval = 0):
 			import uiToolTip
 			pos_x, pos_y = wndMgr.GetMousePosition()
 			self.tlground = uiToolTip.ItemToolTip()
@@ -14,6 +14,6 @@
 			if open:
 				attrSlot = []
 				for i in xrange(player.ATTRIBUTE_SLOT_MAX_NUM):
-					attrSlot.append(attr[i])
+					attrSlot.append([attrtype[i], attrval[i]])
 				self.tlground.SetToolTipPosition(pos_x + 5, pos_y - 5)
 				self.tlground.AddItemData(itemvnum, metinslot, attrSlot)
