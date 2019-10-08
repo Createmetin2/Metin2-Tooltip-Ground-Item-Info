eb345e268267474ec6614f58c6d88569b98b997f
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-08 21:51:37 +0300

    Add files via upload

--- 1.Svn/Client/UserInterface/PythonPlayer.cpp
+++ 1.Svn/Client/UserInterface/PythonPlayer.cpp
@@ -4,27 +4,29 @@
 		s_dwNextTCPTime=dwCurTime + 500;
 		
 ///Add
-	#ifdef TOOLTIP_GROUND_ITEM
-		if (rclick) {
+#ifdef TOOLTIP_GROUND_ITEM
+		if (rclick) 
+		{
 			CItemData * pItemData;
-			if (!CItemManager::Instance().GetItemDataPointer(CPythonItem::Instance().GetVirtualNumberOfGroundItem(dwIID), &pItemData))
-			{
+			if (!CItemManager::Instance().GetItemDataPointer(CPythonItem::Instance().GetVirtualNumberOfGroundItem(dwIID), &pItemData)) {
 				Tracenf("CPythonPlayer::SendClickItemPacket(dwIID=%d) : Non-exist item.", dwIID);
 				return;
 			}
-			PyCallClassMemberFunc(m_ppyGameWindow, "ShowItemFromClient", Py_BuildValue("ii[lll][[ii][ii][ii][ii][ii][ii][ii]]", TRUE, pItemData->GetTable()->dwVnum, 
-				pItemData->GetSockets(0), pItemData->GetSockets(1), pItemData->GetSockets(2), 
-				pItemData->GetAttrType(0), pItemData->GetAttrVal(0),
-				pItemData->GetAttrType(1), pItemData->GetAttrVal(1),
-				pItemData->GetAttrType(2), pItemData->GetAttrVal(2),
-				pItemData->GetAttrType(3), pItemData->GetAttrVal(3),
-				pItemData->GetAttrType(4), pItemData->GetAttrVal(4),
-				pItemData->GetAttrType(5), pItemData->GetAttrVal(5),
-				pItemData->GetAttrType(6), pItemData->GetAttrVal(6)
-			));
+			PyObject *sockets = PyTuple_New(ITEM_SOCKET_SLOT_MAX_NUM);
+			PyObject *attrtype = PyTuple_New(ITEM_ATTRIBUTE_SLOT_MAX_NUM);
+			PyObject *attrval = PyTuple_New(ITEM_ATTRIBUTE_SLOT_MAX_NUM);
+			for (auto i = 0; i < ITEM_SOCKET_SLOT_MAX_NUM; i++)
+				PyTuple_SetItem(sockets, i, PyInt_FromLong(pItemData->GetSockets(i)));
+			for (auto i = 0; i < ITEM_ATTRIBUTE_SLOT_MAX_NUM; i++) {
+				PyTuple_SetItem(attrtype, i, PyInt_FromLong(pItemData->GetAttrType(i)));
+				PyTuple_SetItem(attrval, i, PyInt_FromLong(pItemData->GetAttrVal(i)));
+			}
+			PyCallClassMemberFunc(m_ppyGameWindow, "ShowItemFromClient", Py_BuildValue("iiOOO", TRUE, pItemData->GetTable()->dwVnum, sockets, attrtype, attrval));
+			for (auto& del : { sockets, attrtype, attrval })
+				Py_DECREF(del);
 			return;
 		}
-	#endif
+#endif
 
 //Find
 void CPythonPlayer::SendClickItemPacket(DWORD dwIID)
