74ca30c5f5baf6c37ce2018aa499567de43271a3
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-10 12:35:56 +0300

    Add files via upload

--- 2.Client/root/game.py
+++ 2.Client/root/game.py
@@ -6,6 +6,7 @@
 			self.tlground = None
 			self.xTooltipGroundItemStart = 0
 			self.yTooltipGroundItemStart = 0
+			self.TooltipGroundItemId = 0
 			
 #Find in: def Close(self):
 		self.affectShower = None
@@ -17,6 +18,7 @@
 				self.tlground = None
 			self.xTooltipGroundItemStart = 0
 			self.yTooltipGroundItemStart = 0
+			self.TooltipGroundItemId = 0
 			
 #Find
 		self.interface.BUILD_OnUpdate()
@@ -35,16 +37,20 @@
 		
 #Add
 	if app.TOOLTIP_GROUND_ITEM:
-		def ShowItemFromClient(self, open, itemvnum = 0, metinslot = 0, attrtype = 0, attrval = 0):
+		def ShowItemFromClient(self, open, itemvnum = 0, metinslot = 0, attrtype = 0, attrval = 0, id = 0):
 			import uiToolTip
 			if self.tlground:
-				self.tlground.Hide()
-				self.tlground = None
+				if open and self.TooltipGroundItemId = id: # check if same item
+					return
+				else:
+					self.tlground.Hide()
+					self.tlground = None
 			if open:
 				pos_x, pos_y = wndMgr.GetMousePosition()
 				(self.xTooltipGroundItemStart, self.yTooltipGroundItemStart, z) = player.GetMainCharacterPosition()
 				self.tlground = uiToolTip.ItemToolTip()
 				self.tlground.ClearToolTip()
+				self.TooltipGroundItemId = id
 				attrSlot = []
 				for i in xrange(player.ATTRIBUTE_SLOT_MAX_NUM):	attrSlot.append([attrtype[i], attrval[i]])
 				self.tlground.SetToolTipPosition(pos_x + 5, pos_y - 5)
