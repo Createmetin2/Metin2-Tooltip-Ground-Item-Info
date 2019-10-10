ed57581ab57ed24e3d16e1de62972d5232fa5c38
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-10 13:00:32 +0300

    Add files via upload

--- 1.Svn/Client/GameLib/ItemData.cpp
+++ 1.Svn/Client/GameLib/ItemData.cpp
@@ -6,7 +6,7 @@ void CItemData::SetItemTableData(TItemTable * pItemTable)
 
 ///Add
 #ifdef TOOLTIP_GROUND_ITEM
-void CItemData::SetSocAttr(const std::vector<long>& socket, const std::vector<BYTE>& attrtypes, const std::vector<short>& attrvals, const DWORD& _itemid)
+void CItemData::SetSocAttr(const std::vector<long>& socket, const std::vector<BYTE>& attrtypes, const std::vector<short>& attrvals)
 {
 	sockets.clear();
 	attrtype.clear();
@@ -14,7 +14,6 @@ void CItemData::SetSocAttr(const std::vector<long>& socket, const std::vector<BY
 	sockets = socket;
 	attrtype = attrtypes;
 	attrval = attrvals;
-	ItemId = _itemid;
 }
 #endif
 
--- 1.Svn/Client/GameLib/ItemData.h
+++ 1.Svn/Client/GameLib/ItemData.h
@@ -3,11 +3,10 @@
 		
 ///Add
 #ifdef TOOLTIP_GROUND_ITEM
-		void SetSocAttr(const std::vector<long>& socket, const std::vector<BYTE>& attrtypes, const std::vector<short>& attrvals, const DWORD& _itemid);
+		void SetSocAttr(const std::vector<long>& socket, const std::vector<BYTE>& attrtypes, const std::vector<short>& attrvals);
 		long GetSockets(int i) { return sockets[i]; };
 		BYTE GetAttrType(int i) { return attrtype[i]; };
 		short GetAttrVal(int i) { return attrval[i]; };
-		DWORD GetItemId() { return ItemId; };
 #endif
 
 //Find
@@ -18,5 +17,4 @@
 		std::vector<long> sockets;
 		std::vector<BYTE> attrtype;
 		std::vector<short> attrval;
-		DWORD ItemId;
 #endif
