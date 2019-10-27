5bbd109a0dcdea56e3f3c1a614e0f8c47b0d44b9
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-27 15:53:06 +0300

    Update item.cpp

--- 1.Svn/Server/game/src/item.cpp
+++ 1.Svn/Server/game/src/item.cpp
@@ -3,6 +3,6 @@
 	
 ///Add
 #if defined(TOOLTIP_GROUND_ITEM)
-	thecore_memcpy(pack.alSockets, m_alSockets, sizeof(pack.alSockets));
+	thecore_memcpy(pack.alSockets, GetSockets(), sizeof(pack.alSockets));
 	thecore_memcpy(pack.aAttr, GetAttributes(), sizeof(pack.aAttr));
 #endif
