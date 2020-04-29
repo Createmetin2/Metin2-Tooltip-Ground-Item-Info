7367d5e17286e0645bc53c038c76b0e0c66333c1
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-04-29 18:50:48 +0300

    Update Tooltip.h

--- 1.Svn/Client/UserInterface/Tooltip.h
+++ 1.Svn/Client/UserInterface/Tooltip.h
@@ -2,10 +2,12 @@
 
 #if defined(TOOLTIP_GROUND_ITEM)
 #include <array>
+static const bool ENABLE_OWNERSHIP = true;
 class TooltipData
 {
 public:
-	TooltipData(const long* socket, const TPlayerItemAttribute* attr) {
+	TooltipData(const long* socket, const TPlayerItemAttribute* attr)
+	{	
 		for (size_t i = T_SOCKET; i < T_MAX; i++)
 			Tooltip.at(i) = PyList_New(0);
 
@@ -15,15 +17,25 @@ public:
 		for (size_t i = 0; i < ITEM_ATTRIBUTE_SLOT_MAX_NUM; i++)
 			PyList_Append(Tooltip.at(T_ATTR), Py_BuildValue("[ii]", attr[i].bType, attr[i].sValue));
 	}
-	~TooltipData() {
+	~TooltipData() 
+	{
 		for (const auto& v : Tooltip)
 			Py_DECREF(v);
 	}
 
+	void SetOwner(const char* name)
+	{
+		if (ENABLE_OWNERSHIP && Owner.empty())
+			Owner.assign(name);
+	}
+
 	enum : size_t { T_SOCKET, T_ATTR, T_MAX };
+
+	const auto& GetOwner() const { return Owner; }
 	auto operator[] (const size_t idx) const { return Tooltip.at(idx); };
 
 private:
 	std::array<PyObject*, T_MAX> Tooltip;
+	std::string Owner; // will be protected when ownership time is over
 };
 #endif
